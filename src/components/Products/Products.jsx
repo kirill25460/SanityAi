febfaa18a8c36feaf84debc786f5327e5ccab3e9
Vlad Melnik <vmelniki2828@gmail.com>
2024-12-29 06:41:09 +0200

    edits

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,19 +1,62 @@
-import { MainContainer,LinkProduct,CardWrap,CardList,CardItem,BigCard,SmallCard} from "./Products.styled";
-import bigCard from "../../images/big_card.png";
-import smallCard from "../../images/small_card.png";
+import {
+  MainContainer,
+  LinkProduct,
+  CardWrap,
+  CardList,
+  CardItem,
+  BigCard,
+  SmallCard,
+  ImageBlock,
+  ImageBlockText,
+} from './Products.styled';
+import bigCard from '../../images/big_card.png';
+import smallCard from '../../images/small_card.png';
+import block from '../../images/back.png';
+
 export const Products = () => {
-    return (
-   <MainContainer id="Products">
-<LinkProduct>Sanity / Products</LinkProduct>
-<CardWrap>
-    <CardList>
-        <CardItem><BigCard src={bigCard} alt="bigCard" /><SmallCard src={smallCard} alt="SANITYAI"/></CardItem>
-        <CardItem></CardItem>
-        <CardItem></CardItem>
-        <CardItem></CardItem>
-    </CardList>
-</CardWrap>
-   </MainContainer>
-    );
-  };
-  
+  return (
+    <MainContainer id="Products">
+      <LinkProduct>Sanity / Products</LinkProduct>
+      <CardWrap>
+        <CardList>
+          <CardItem>
+            <ImageBlock src={block} position="top" height="418px" />
+            <ImageBlock src={block} position="bottom" height="65px">
+              <ImageBlockText>Call Center QA</ImageBlockText>
+            </ImageBlock>
+          </CardItem>
+          <CardItem>
+            <ImageBlock src={block} position="top" height="65px">
+              <ImageBlockText>Call Center QA</ImageBlockText>
+            </ImageBlock>
+            <ImageBlock src={block} position="bottom" height="418px" />
+          </CardItem>
+          <CardItem>
+            <ImageBlock src={block} position="top" height="418px" />
+            <ImageBlock src={block} position="bottom" height="65px">
+              <ImageBlockText>Call Center QA</ImageBlockText>
+            </ImageBlock>
+          </CardItem>
+          <CardItem>
+            <ImageBlock src={block} position="top" height="65px">
+              <ImageBlockText>Call Center QA</ImageBlockText>
+            </ImageBlock>
+            <ImageBlock src={block} position="bottom" height="418px" />
+          </CardItem>
+          <CardItem>
+            <ImageBlock src={block} position="top" height="418px" />
+            <ImageBlock src={block} position="bottom" height="65px">
+              <ImageBlockText>Call Center QA</ImageBlockText>
+            </ImageBlock>
+          </CardItem>
+          <CardItem>
+            <ImageBlock src={block} position="top" height="65px">
+              <ImageBlockText>Call Center QA</ImageBlockText>
+            </ImageBlock>
+            <ImageBlock src={block} position="bottom" height="418px" />
+          </CardItem>
+        </CardList>
+      </CardWrap>
+    </MainContainer>
+  );
+};
--- src/components/Products/Products.styled.js
+++ src/components/Products/Products.styled.js
@@ -1,57 +1,101 @@
 import styled from 'styled-components';
 
-
 export const MainContainer = styled.div`
-
-padding-left: 15px;
-padding-right: 15px;
-height:100vh;
+  padding-left: 15px;
+  padding-right: 15px;
+  height: 100vh;
 `;
 
 export const LinkProduct = styled.h3`
-font-family: 'Montserrat';
-font-style: normal;
-font-weight: 600;
-font-size: 15px;
-line-height: 18px;
-display: flex;
-align-items: center;
+  font-family: 'Montserrat';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 15px;
+  line-height: 18px;
+  display: flex;
+  align-items: center;
 
-color: #000000;
-margin-top: 140px;
-margin-left: 60px;
-margin-bottom: 60px;
+  color: #000000;
+  margin-top: 140px;
+  margin-left: 60px;
+  margin-bottom: 60px;
 `;
 
 export const CardWrap = styled.div`
-display:flex;
-justify-content: center;
-align-items: center;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  margin: 0 auto;
 `;
 
 export const CardList = styled.ul`
-margin: 0;
-
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  align-items: center;
+  gap: 30px;
+  margin: 0;
+  padding: 0;
+  list-style: none;
 `;
 
-export const CardItem = styled.li`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+export const CardItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 309px; // Ширина карточки
+  margin: 10px;
+  gap: 30px;
 `;
 
-export const BigCard = styled.img`
-width: 309px;
-height: 418px;
-background: linear-gradient(180deg, rgba(0, 0, 0, 0) 64.35%, rgba(0, 0, 0, 0.37) 100%), url(image.png);
-border-radius: 5px;
+// export const BigCard = styled.img`
+//   width: 309px;
+//   height: 418px;
+//   background: linear-gradient(
+//       180deg,
+//       rgba(0, 0, 0, 0) 64.35%,
+//       rgba(0, 0, 0, 0.37) 100%
+//     ),
+//     url(image.png);
+//   border-radius: 5px;
+// `;
 
+// export const SmallCard = styled.img`
+//   margin-top: 15px;
+//   width: 309px;
+//   height: 65px;
+//   background: linear-gradient(
+//       180deg,
+//       rgba(0, 0, 0, 0) -310.46%,
+//       rgba(0, 0, 0, 0.37) 100%
+//     ),
+//     url(image.png);
+//   border-radius: 5px;
+// `;
+
+export const ImageBlock = styled.div`
+  width: 309px;
+  height: ${props => props.height || '50%'};
+  background-image: url(${props => props.src});
+  background-position: ${props => props.position};
+  background-size: cover;
+  background-repeat: no-repeat;
+  margin: 0; /* убираем маргин для отсутствия отступов */
+  display: flex;
+  align-items: center;
 `;
 
-export const SmallCard = styled.img`
-margin-top: 15px;
-width: 309px;
-height: 65px;
-background: linear-gradient(180deg, rgba(0, 0, 0, 0) -310.46%, rgba(0, 0, 0, 0.37) 100%), url(image.png);
-border-radius: 5px;
+export const ImageBlockText = styled.p`
+  font-family: 'Montserrat';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 15px;
+  line-height: 18px;
+  display: flex;
+  align-items: center;
+
+  color: #ffffff;
+  margin: 0;
+  padding-left: 30px;
 `;
