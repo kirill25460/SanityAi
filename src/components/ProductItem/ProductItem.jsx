71b0710f571b78e5ca0a4f09bda96a59cb07784f
Vlad Melnik <vmelniki2828@gmail.com>
2025-01-04 06:41:59 +0200

    edits

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -1,15 +1,64 @@
-import { ProductInfoLeftContainer, ProductInfoContainer, ProductMainContainer, ProductMainName, ProductMainTitle, ProductInfoLeftText } from "./ProductItem.styled";
+import {
+  ProductInfoLeftContainer,
+  ProductInfoContainer,
+  ProductMainContainer,
+  ProductMainName,
+  ProductMainTitle,
+  ProductInfoLeftText,
+  ProductInfoLeftBotContainer,
+  Block280x180,
+  Block280x180TopText,
+  Block280x180BotText,
+  Block240x180,
+  Block240x180Text,
+  Block160x120,
+  Block160x120TopText,
+  Block160x120BotText,
+  Block360x120,
+  Block360x120TopText,
+  Block360x120BotText,
+  TopRow,
+  BotRow,
+} from './ProductItem.styled';
 
 const ProductItem = () => {
-    return (<ProductMainContainer>
-        <ProductMainTitle>Sanity / Products / </ProductMainTitle>
-        <ProductMainName>Callbuddy</ProductMainName>
-        <ProductInfoContainer>
-            <ProductInfoLeftContainer>
-                <ProductInfoLeftText>Вступительный текст, который описывает продукт. Вступительный текст, который описывает продукт. Вступительный текст, который описывает продукт. Вступительный текст, который описывает продукт. Вступительный текст.</ProductInfoLeftText>
-            </ProductInfoLeftContainer>
-        </ProductInfoContainer>
-    </ProductMainContainer>)
-}
+  return (
+    <ProductMainContainer>
+      <ProductMainTitle>Sanity / Products / </ProductMainTitle>
+      <ProductMainName>Callbuddy</ProductMainName>
+      <ProductInfoContainer>
+        <ProductInfoLeftContainer>
+          <ProductInfoLeftText>
+            Вступительный текст, который описывает продукт. Вступительный текст,
+            который описывает продукт. Вступительный текст, который описывает
+            продукт. Вступительный текст, который описывает продукт.
+            Вступительный текст.
+          </ProductInfoLeftText>
+          <ProductInfoLeftBotContainer>
+            <TopRow>
+              <Block280x180>
+                <Block280x180TopText>98%</Block280x180TopText>
+                <Block280x180BotText>Корректных проверок</Block280x180BotText>
+              </Block280x180>
+              <Block240x180>
+                <Block240x180Text>Надо придумать)</Block240x180Text>
+              </Block240x180>
+            </TopRow>
+            <BotRow>
+              <Block160x120>
+                <Block160x120TopText>5/5</Block160x120TopText>
+                <Block160x120BotText>рейтинг</Block160x120BotText>
+              </Block160x120>
+              <Block360x120>
+                <Block360x120TopText>База клиентов</Block360x120TopText>
+                <Block360x120BotText>Более 300тыс.</Block360x120BotText>
+              </Block360x120>
+            </BotRow>
+          </ProductInfoLeftBotContainer>
+        </ProductInfoLeftContainer>
+      </ProductInfoContainer>
+    </ProductMainContainer>
+  );
+};
 
 export default ProductItem;
--- src/components/ProductItem/ProductItem.styled.jsx
+++ src/components/ProductItem/ProductItem.styled.jsx
@@ -6,38 +6,166 @@ export const ProductMainContainer = styled.div`
 `;
 
 export const ProductMainTitle = styled.h2`
-font-family: 'Montserrat';
-font-style: normal;
-font-weight: 600;
-font-size: 15px;
-line-height: 18px;
-color: #000000;
-margin: 0 0 15px 0;
-`
+  font-family: 'Montserrat';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 15px;
+  line-height: 18px;
+  color: #000000;
+  margin: 0 0 15px 0;
+`;
 
 export const ProductMainName = styled.h3`
-font-family: 'Montserrat';
-font-style: normal;
-font-weight: 500;
-font-size: 50px;
-line-height: 61px;
-color: #000000;
-margin: 0 0 30px 0;
-`
+  font-family: 'Montserrat';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 50px;
+  line-height: 61px;
+  color: #000000;
+  margin: 0 0 30px 0;
+`;
 
 export const ProductInfoContainer = styled.div`
-    display:flex;
-`
+  display: flex;
+`;
 
-export const ProductInfoLeftContainer = styled.div`
-
-`
+export const ProductInfoLeftContainer = styled.div``;
 export const ProductInfoLeftText = styled.p`
-width: 570px;
-font-family: 'Montserrat';
-font-style: normal;
-font-weight: 400;
-font-size: 29px;
-line-height: 35px;
-color: #000000;
+  width: 570px;
+  font-family: 'Montserrat';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 29px;
+  line-height: 35px;
+  color: #000000;
+`;
+
+export const ProductInfoLeftBotContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 50px;
+`;
+
+export const TopRow = styled.div`
+  display: flex;
+  gap: 50px;
 `
+
+export const BotRow = styled.div`
+  display: flex;
+  gap: 50px;
+`
+
+export const Block280x180 = styled.div`
+  padding: 26px 20px;
+  width: 240px;
+  height: 132px;
+  background: #ffffff;
+  border: 2px solid #000000;
+  border-radius: 10px;
+`;
+
+export const Block280x180TopText = styled.h4`
+  margin: 0;
+  font-family: 'Montserrat';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 50px;
+  line-height: 61px;
+  color: #000000;
+`;
+
+export const Block280x180BotText = styled.p`
+  margin: 0;
+  font-family: 'Montserrat';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 33px;
+  line-height: 40px;
+  color: #000000;
+`;
+
+export const Block240x180 = styled.div`
+  width: 240px;
+  height: 180px;
+  background: #ffffff;
+  border: 2px solid #000000;
+  border-radius: 10px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+`;
+
+export const Block240x180Text = styled.p`
+  font-family: 'Montserrat';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 33px;
+  line-height: 40px;
+  color: #000000;
+`;
+
+export const Block160x120 = styled.div`
+  width: 160px;
+  height: 120px;
+  background: #ffffff;
+  border: 2px solid #000000;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Block160x120TopText = styled.h4`
+  margin: 0;
+  font-family: 'Montserrat';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 50px;
+  line-height: 61px;
+  color: #000000;
+`;
+
+export const Block160x120BotText = styled.p`
+  margin: 0;
+  font-family: 'Montserrat';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 24px;
+  color: #000000;
+`;
+
+export const Block360x120 = styled.div`
+  padding-left: 22px;
+  width: 338px;
+  height: 120px;
+  background: #ffffff;
+  border: 2px solid #000000;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  justify-content: center;
+`;
+
+export const Block360x120TopText = styled.p`
+  margin: 0;
+  font-family: 'Montserrat';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 33px;
+  line-height: 40px;
+  color: #000000;
+`;
+
+export const Block360x120BotText = styled.p`
+  margin: 0;
+  font-family: 'Montserrat';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 36px;
+  line-height: 44px;
+  color: #000000;
+`;
