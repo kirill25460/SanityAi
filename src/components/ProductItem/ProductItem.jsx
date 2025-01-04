1da63ed34912e0cdbb3bb152a2013a0ae09eedb1
vmelniki2828 <vmelniki2828@gmail.com>
2025-01-04 10:07:18 +0200

    edits

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -21,49 +21,119 @@ import {
   BotRow,
   ProductInfoRightContainer,
   RightTopImg,
+  RightBotContainer,
+  RightBotImgSmall,
+  RightBotImgBig,
+  HowItWorksMainText,
+  HowItWorksMainContainer,
+  HowItWorksImg,
+  HowItWorksContainer,
+  HowItWorksBlock,
+  HowItWorksSubText,
+  WhyWeMainContainer,
+  WhyWeMainText,
+  WhyWeInsideContainer,
+  WhyWeSubContainer,
+  WhyWeBlock,
+  CenteredNumber,
+  WhyWeBlockBig,
+  CenteredNumberBig,
+  WhyWeSubText,
 } from './ProductItem.styled';
-import back from '../../images/back.png'
+import back from '../../images/back.png';
 
 const ProductItem = () => {
   return (
-    <ProductMainContainer>
-      <ProductMainTitle>Sanity / Products / </ProductMainTitle>
-      <ProductMainName>Callbuddy</ProductMainName>
-      <ProductInfoContainer>
-        <ProductInfoLeftContainer>
-          <ProductInfoLeftText>
-            Вступительный текст, который описывает продукт. Вступительный текст,
-            который описывает продукт. Вступительный текст, который описывает
-            продукт. Вступительный текст, который описывает продукт.
-            Вступительный текст.
-          </ProductInfoLeftText>
-          <ProductInfoLeftBotContainer>
-            <TopRow>
-              <Block280x180>
-                <Block280x180TopText>98%</Block280x180TopText>
-                <Block280x180BotText>Корректных проверок</Block280x180BotText>
-              </Block280x180>
-              <Block240x180>
-                <Block240x180Text>Надо придумать)</Block240x180Text>
-              </Block240x180>
-            </TopRow>
-            <BotRow>
-              <Block160x120>
-                <Block160x120TopText>5/5</Block160x120TopText>
-                <Block160x120BotText>рейтинг</Block160x120BotText>
-              </Block160x120>
-              <Block360x120>
-                <Block360x120TopText>База клиентов</Block360x120TopText>
-                <Block360x120BotText>Более 300тыс.</Block360x120BotText>
-              </Block360x120>
-            </BotRow>
-          </ProductInfoLeftBotContainer>
-        </ProductInfoLeftContainer>
-        <ProductInfoRightContainer>
-            <RightTopImg src={back}/>
-        </ProductInfoRightContainer>
-      </ProductInfoContainer>
-    </ProductMainContainer>
+    <>
+      <ProductMainContainer>
+        <ProductMainTitle>Sanity / Products / </ProductMainTitle>
+        <ProductMainName>Callbuddy</ProductMainName>
+        <ProductInfoContainer>
+          <ProductInfoLeftContainer>
+            <ProductInfoLeftText>
+              Вступительный текст, который описывает продукт. Вступительный
+              текст, который описывает продукт. Вступительный текст, который
+              описывает продукт. Вступительный текст, который описывает продукт.
+              Вступительный текст.
+            </ProductInfoLeftText>
+            <ProductInfoLeftBotContainer>
+              <TopRow>
+                <Block280x180>
+                  <Block280x180TopText>98%</Block280x180TopText>
+                  <Block280x180BotText>Корректных проверок</Block280x180BotText>
+                </Block280x180>
+                <Block240x180>
+                  <Block240x180Text>Надо придумать)</Block240x180Text>
+                </Block240x180>
+              </TopRow>
+              <BotRow>
+                <Block160x120>
+                  <Block160x120TopText>5/5</Block160x120TopText>
+                  <Block160x120BotText>рейтинг</Block160x120BotText>
+                </Block160x120>
+                <Block360x120>
+                  <Block360x120TopText>База клиентов</Block360x120TopText>
+                  <Block360x120BotText>Более 300тыс.</Block360x120BotText>
+                </Block360x120>
+              </BotRow>
+            </ProductInfoLeftBotContainer>
+          </ProductInfoLeftContainer>
+          <ProductInfoRightContainer>
+            <RightTopImg src={back} />
+            <RightBotContainer>
+              <RightBotImgSmall src={back} />
+              <RightBotImgBig src={back} />
+            </RightBotContainer>
+          </ProductInfoRightContainer>
+        </ProductInfoContainer>
+        <HowItWorksMainText>Как это работает?</HowItWorksMainText>
+        <HowItWorksMainContainer>
+          <HowItWorksImg src={back} />
+          <HowItWorksContainer>
+            <HowItWorksBlock>
+              <HowItWorksSubText>
+                Вступительный текст, который описывает продукт. Вступительный
+                текст, который описывает продукт.
+              </HowItWorksSubText>
+            </HowItWorksBlock>
+            <HowItWorksBlock>
+              <HowItWorksSubText>
+                Вступительный текст, который описывает продукт. Вступительный
+                текст, который описывает продукт.
+              </HowItWorksSubText>
+            </HowItWorksBlock>
+          </HowItWorksContainer>
+        </HowItWorksMainContainer>
+      </ProductMainContainer>
+      <WhyWeMainContainer>
+        <WhyWeInsideContainer>
+          <WhyWeMainText>Почему это именно то что вам нужно?</WhyWeMainText>
+          <WhyWeSubContainer>
+            <WhyWeBlock>
+              <CenteredNumber>1</CenteredNumber>
+              <HowItWorksSubText>
+                Вступительный текст, который описывает продукт. Вступительный
+                текст, который описывает продукт.
+              </HowItWorksSubText>
+            </WhyWeBlock>
+            <WhyWeBlock>
+              <CenteredNumber>2</CenteredNumber>
+              <HowItWorksSubText>
+                Вступительный текст, который описывает продукт. Вступительный
+                текст, который описывает продукт.
+              </HowItWorksSubText>
+            </WhyWeBlock>
+          </WhyWeSubContainer>
+          <WhyWeBlockBig>
+            <CenteredNumberBig>3</CenteredNumberBig>
+            <WhyWeSubText>
+              Вступительный текст, который описывает продукт. Вступительный
+              текст, который описывает продукт.
+            </WhyWeSubText>
+          </WhyWeBlockBig>
+        </WhyWeInsideContainer>
+      </WhyWeMainContainer>
+    </>
   );
 };
 
--- src/components/ProductItem/ProductItem.styled.jsx
+++ src/components/ProductItem/ProductItem.styled.jsx
@@ -28,19 +28,32 @@ export const ProductMainName = styled.h3`
 export const ProductInfoContainer = styled.div`
   display: flex;
   gap: 50px;
+  margin-bottom: 100px;
 `;
 
-export const ProductInfoLeftContainer = styled.div``;
-export const ProductInfoRightContainer = styled.div``;
+export const ProductInfoLeftContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 50px;
+`;
+
+export const ProductInfoRightContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 50px;
+`;
+
 export const ProductInfoLeftText = styled.p`
+  display: block;
   width: 570px;
+  height: 250px;
   font-family: 'Montserrat';
   font-style: normal;
   font-weight: 400;
   font-size: 29px;
   line-height: 35px;
   color: #000000;
-  margin: 0 0 50px 0;
+  margin: 0;
 `;
 
 export const ProductInfoLeftBotContainer = styled.div`
@@ -62,7 +75,7 @@ export const BotRow = styled.div`
 export const Block280x180 = styled.div`
   padding: 26px 20px;
   width: 236px;
-  height: 132px;
+  height: 124px;
   background: #ffffff;
   border: 2px solid #000000;
   border-radius: 10px;
@@ -90,7 +103,7 @@ export const Block280x180BotText = styled.p`
 
 export const Block240x180 = styled.div`
   width: 236px;
-  height: 180px;
+  height: 176px;
   background: #ffffff;
   border: 2px solid #000000;
   border-radius: 10px;
@@ -110,7 +123,7 @@ export const Block240x180Text = styled.p`
 
 export const Block160x120 = styled.div`
   width: 156px;
-  height: 120px;
+  height: 116px;
   background: #ffffff;
   border: 2px solid #000000;
   border-radius: 10px;
@@ -143,7 +156,7 @@ export const Block160x120BotText = styled.p`
 export const Block360x120 = styled.div`
   padding-left: 22px;
   width: 334px;
-  height: 120px;
+  height: 116px;
   background: #ffffff;
   border: 2px solid #000000;
   border-radius: 10px;
@@ -174,7 +187,186 @@ export const Block360x120BotText = styled.p`
 `;
 
 export const RightTopImg = styled.img`
+  display: block;
   width: 700px;
   height: 250px;
   border-radius: 5px;
+`;
+
+export const RightBotImgSmall = styled.img`
+  width: 300px;
+  height: 350px;
+  border-radius: 5px;
+`;
+
+export const RightBotImgBig = styled.img`
+  width: 350px;
+  height: 350px;
+  border-radius: 5px;
+`;
+
+export const RightBotContainer = styled.div`
+  display: flex;
+  gap: 50px;
+`;
+
+export const HowItWorksMainText = styled.h4`
+  margin: 0 0 60px 0;
+  font-family: 'Montserrat';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 50px;
+  line-height: 61px;
+  color: #000000;
+`;
+
+export const HowItWorksMainContainer = styled.div`
+  display: flex;
+  gap: 60px;
+  margin-bottom: 100px;
+`;
+
+export const HowItWorksImg = styled.img`
+  width: 645px;
+  height: 364px;
+  border-radius: 5px;
+`;
+
+export const HowItWorksContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+export const HowItWorksBlock = styled.div`
+  width: 611px;
+  height: 168px;
+  background: #ffffff;
+  border: 2px solid #000000;
+  border-radius: 10px;
+`;
+
+export const HowItWorksSubText = styled.p`
+  margin: 0;
+  padding: 34px;
+  font-family: 'Montserrat';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 29px;
+  line-height: 35px;
+  color: #000000;
+`;
+
+export const WhyWeMainContainer = styled.div`
+  width: 100%;
+  background-color: #000;
+  color: #fff;
+  padding-bottom: 100px;
+`;
+
+export const WhyWeInsideContainer = styled.div`
+  width: 1320px;
+  margin: 60px auto 0;
+`;
+
+export const WhyWeMainText = styled.h2`
+  margin: 0;
+  padding: 50px 0;
+  font-family: 'Montserrat';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 50px;
+  line-height: 61px;
+  color: #ffffff;
+  width: 1320px;
+`;
+
+export const WhyWeSubContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 90px;
+  margin-bottom: 65px;
+`;
+
+export const WhyWeBlock = styled.div`
+  width: 615px;
+  height: 172px;
+
+  background: #ffffff;
+  border: 2px solid #000000;
+  border-radius: 10px;
+  position: relative; /* Устанавливаем для вложенных абсолютных элементов */
+  display: flex;
+  justify-content: center; /* Центрируем текст внутри блока */
+  align-items: center; /* Центрируем по вертикали */
+`;
+
+export const WhyWeBlockBig = styled.div`
+  margin: 0 auto;
+  width: 922px;
+  height: 258px;
+
+  background: #ffffff;
+  border: 2px solid #000000;
+  border-radius: 10px;
+  position: relative; /* Устанавливаем для вложенных абсолютных элементов */
+  display: flex;
+  justify-content: center; /* Центрируем текст внутри блока */
+  align-items: center; /* Центрируем по вертикали */
+`;
+
+export const CenteredNumber = styled.div`
+  position: absolute;
+  background: #ffffff; /* Устанавливаем белый фон */
+  border: 2px solid #000; /* Добавляем границу, если нужно */
+  border-radius: 5px; /* Делаем блок круглым */
+  width: 50px;
+  height: 50px;
+  display: flex;
+  align-items: center; /* Центрируем цифру по вертикали */
+  justify-content: center; /* Центрируем цифру по горизонтали */
+  left: 50%; /* Центрируем по горизонтали */
+  top: 0; /* Устанавливаем сверху */
+  transform: translate(-50%, -50%); /* Центруем относительно самого блока */
+  color: #000;
+  font-family: 'Montserrat';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 29px;
+  line-height: 35px;
+  color: #000000;
+  z-index: 1; /* Поверх текста */
+`;
+
+export const CenteredNumberBig = styled.div`
+  position: absolute;
+  background: #ffffff; /* Устанавливаем белый фон */
+  border: 2px solid #000; /* Добавляем границу, если нужно */
+  border-radius: 5px; /* Делаем блок круглым */
+  width: 75px;
+  height: 75px;
+  display: flex;
+  align-items: center; /* Центрируем цифру по вертикали */
+  justify-content: center; /* Центрируем цифру по горизонтали */
+  left: 50%; /* Центрируем по горизонтали */
+  top: 0; /* Устанавливаем сверху */
+  transform: translate(-50%, -50%); /* Центруем относительно самого блока */
+  font-family: 'Montserrat';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 43.5px;
+  line-height: 53px;
+  color: #000000;
+  z-index: 1; /* Поверх текста */
+`;
+
+export const WhyWeSubText = styled.p`
+  margin: 0;
+  padding: 50px;
+  font-family: 'Montserrat';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 43px;
+  line-height: 52px;
+  color: #000000;
 `;
